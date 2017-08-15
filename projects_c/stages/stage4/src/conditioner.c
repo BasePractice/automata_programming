ede876f3e1f1becf38a493e9d45122f7346039ad
Андрей <viruszold@mail.ru>
2017-08-15 17:04:28 +0300

    Эмуляция температурного датчика

---
+++ projects_c/stages/stage4/src/conditioner.c
@@ -0,0 +1,79 @@
+#include "conditioner.h"
+
+static enum State
+check_errors(enum State state, struct ConditionerInter *cond, struct StorageInter *storage) {
+    if ((*storage->base.errors)()) {
+        return _StorageError;
+    }
+    if ((*cond->base.errors)()) {
+        return _CondError;
+    }
+    return state;
+}
+
+int
+business_fsm(enum State state, struct ConditionerInter *cond, struct StorageInter *storage) {
+    int ret = ERR_OK;
+    do {
+        /** Для тестирования. Эмуляция внешнего управления*/
+        if (cond->is_running && !cond->is_running(state)) {
+            state = _PowerOff;
+        }
+        switch (state) {
+            case _PowerOff: {
+                storage->property_store();
+                state = check_errors(state, cond, storage);
+                break;
+            }
+            case _PowerOn: {
+                storage->property_load();
+                state = check_errors(_Control, cond, storage);
+                break;
+            }
+            case _Waiting: {
+                int wait = storage->property_get_int(WaitingIdle);
+                cond->process_engine(Waiting, wait);
+                state = check_errors(_Control, cond, storage);
+                break;
+            }
+            case _Heating: {
+                int wait = storage->property_get_int(WaitingHeating);
+                cond->process_engine(Heating, wait);
+                state = check_errors(_Control, cond, storage);
+                break;
+            }
+            case _Cooling: {
+                int wait = storage->property_get_int(WaitingCooling);
+                cond->process_engine(Cooling, wait);
+                state = check_errors(_Control, cond, storage);
+                break;
+            }
+            case _Control: {
+                int guard_temperature = storage->property_get_int(Temperature);
+                int current_temperature = cond->get_current_temperature();
+                if (current_temperature > guard_temperature) {
+                    state = _Cooling;
+                } else if (current_temperature < guard_temperature) {
+                    state = _Heating;
+                } else {
+                    state = _Waiting;
+                }
+                state = check_errors(state, cond, storage);
+                break;
+            }
+            case _StorageError: {
+                /** Обработка ошибок */
+                state = _PowerOff;
+                ret = ERR_STORAGE;
+                break;
+            }
+            case _CondError: {
+                /** Обработка ошибок */
+                state = _PowerOff;
+                ret = ERR_CONDITIONER;
+                break;
+            }
+        }
+    } while (state != _PowerOff);
+    return ret;
+}
---
+++ projects_c/stages/stage4/src/conditioner_i.h
@@ -0,0 +1,59 @@
+#ifndef _CONDITIONER_I_H_
+#define _CONDITIONER_I_H_
+
+#if defined(_MSC_VER)
+#include <windows.h>
+#define x_sleep(t)   Sleep(t)
+#else
+#include <unistd.h>
+#define x_sleep(t)   usleep((t) * 1000)
+#endif
+
+/** Параметры кондиционера */
+enum Property {
+    Temperature,
+    WaitingCooling,
+    WaitingHeating,
+    WaitingIdle,
+    _PropertyCount
+};
+
+/** Режим работы кондиционера */
+enum EngineMode {
+    /** Охлаждение */
+    Cooling,
+    /** Нагрев */
+    Heating,
+    /** Ожидание */
+    Waiting
+};
+
+struct BaseInter {
+    int (*errors)();
+};
+
+/** Интерфейс хранилища */
+struct StorageInter {
+    struct BaseInter base;
+    /** Загрузка параметров их хранилища */
+    int (*property_load)();
+    /** Сохранение параметров */
+    int (*property_store)();
+    int  (*property_get_int)(enum Property key);
+    /** Установка параметра по его наименованию */
+    void (*property_set_int)(enum Property key, int value);
+};
+
+/** Основной интерфейс кондициронер */
+struct ConditionerInter {
+    struct BaseInter base;
+    /** Получение температуры помещения */
+    int (*get_current_temperature)();
+    /** Выставление работы с установкой режима работы и времени работы */
+    int (*process_engine)(enum EngineMode mode, int timeout);
+    /** Добавить проверку наличия ошибок */
+
+    int (*is_running)(int state);
+};
+
+#endif
